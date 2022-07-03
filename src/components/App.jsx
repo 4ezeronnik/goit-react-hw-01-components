9fa93e94d4b1b02e328abe3ef27d1dedab4f6063
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-07-03 20:47:17 +0300

    added changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,26 @@
 import Profile from "../components/Profile/ Profile";
+import Statistics from "../components/Statistics/Statistics"
 import user from './user.json';
+import data from './data.json';
 
 export default function App () {
   return (
-  <div>
+    <section>
+      <div>
     <Profile
       username={user.username}
       tag={user.tag}
       location={user.location}
       avatar={user.avatar}
       stats={user.stats}
-    />
-  </div>
+      />
+      </div>
+      <div>
+        <Statistics title="Upload stats" stats={data} /> 
+      </div>
+    </section>
+   
   );
+  
 }
 
