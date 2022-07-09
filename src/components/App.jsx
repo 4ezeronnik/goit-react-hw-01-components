62511e29ae79168fcf41a0638d2236e215bc0760
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-07-09 15:34:19 +0300

    added TransactionHistory

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import Profile from "../components/Profile/ Profile";
 import Statistics from "../components/Statistics/Statistics"
 import FriendList from "../components/FriendList/FriendList";
+import TransactionHistory from "../components/TransactionHistory/TransactionHistory"
 import user from './user.json';
 import data from './data.json';
 import friends from './friends.json';
+import transactions from './transactions.json';
+
 
 
 
@@ -24,6 +27,8 @@ export default function App () {
       
     <FriendList
         friends={friends} />
+      
+      <TransactionHistory items={transactions} />
       
     </>
    
