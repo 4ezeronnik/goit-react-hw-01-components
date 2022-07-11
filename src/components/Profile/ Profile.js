13107482592eed4590e1810d0a9514ad00e5e8bc
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-07-11 10:15:34 +0300

    Update  Profile.js

--- src/components/Profile/ Profile.js
+++ src/components/Profile/ Profile.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import styles from './Profile.module.css';
 
-const Profile = ({ avatar, username, tag, location, stats }) => {
+const Profile = ({ avatar, username, tag, location, stats: {followers, likes, views} }) => {
   return  <div className={styles.profile}>
     <div className={styles.description}>
       <img
@@ -17,15 +17,15 @@ const Profile = ({ avatar, username, tag, location, stats }) => {
     <ul className={styles.stats}>
       <li className={styles.statElement}>
         <span className={styles.label}>Followers</span>
-              <span className={styles.quantity}>{stats.followers}</span>
+              <span className={styles.quantity}>{followers}</span>
       </li>
       <li className={styles.statElement}>
         <span className={styles.label}>Views</span>
-              <span className={styles.quantity}>{stats.views}</span>
+              <span className={styles.quantity}>{views}</span>
       </li>
       <li className={styles.statElement}>
         <span className={styles.label}>Likes</span>
-              <span className={styles.quantity}>{stats.likes}</span>
+              <span className={styles.quantity}>{likes}</span>
       </li>
     </ul>
   </div>
